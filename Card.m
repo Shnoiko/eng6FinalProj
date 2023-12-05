3d239b5d69357cc326289d25edf19d1b1682af76
Shnoiko <nikostrato18@gmail.com>
2023-12-05 10:10:08 -0800

    Add new wild images and update card class

--- Card.m
+++ Card.m
@@ -30,17 +30,17 @@ classdef Card
             %12 = red swap  25 = blue swap  38 = yellow swap
             %
             %
-            %39 = green 0   52 = wild
-            %40 = green 1   53 = pfour
-            %41 = green 2
-            %etc..
-            %48 = green 9
-            %49 = green ptwo
-            %50 = green skip
-            %51 = green swap
+            %39 = green 0   52 = wild   54 = wild red
+            %40 = green 1   53 = pfour  55 = wild blue
+            %41 = green 2               56 = wild yellow
+            %etc..                      57 = wild green
+            %48 = green 9               58 = pfour red
+            %49 = green ptwo            59 = pfour blue
+            %50 = green skip            60 = pfour yellow
+            %51 = green swap            61 = pfour green
 
             %make sure encodedCardNumber is in the correct range
-            if (encodedCardNumber > 53) | (encodedCardNumber < 0)
+            if (encodedCardNumber > 61) || (encodedCardNumber < 0)
                 error("input arg must be an integer less than or equal to 53")
             else
                 obj.encodedCardNumber = encodedCardNumber;
@@ -58,11 +58,41 @@ classdef Card
                 elseif encodedCardNumber <= 51
                     obj.color = 'green';
                     obj.number = encodedCardNumber - 39;
-                else
+                elseif encodedCardNumber <= 53
                     obj.color = 'wild';
                     obj.number = encodedCardNumber - 52;
+                else
+                    %special case for wild cards that have been placed
+                    obj.number = 'wild';
+                    if encodedCardNumber == 54
+                        obj.color = 'red';
+                        obj.imageSource = '/UnoCards/wildred.png';
+                    elseif encodedCardNumber == 55
+                        obj.color = 'blue';
+                        obj.imageSource = '/UnoCards/wildblue.png';
+                    elseif encodedCardNumber == 56
+                        obj.color = 'yellow';
+                        obj.imageSource = '/UnoCards/wildyellow.png';
+                    elseif encodedCardNumber == 57
+                        obj.color = 'green';
+                        obj.imageSource = '/UnoCards/wildgreen.png';
+                    elseif encodedCardNumber == 58
+                        obj.color = 'red';
+                        obj.imageSource = '/UnoCards/wildp4red.png';
+                    elseif encodedCardNumber == 59
+                        obj.color = 'blue';
+                        obj.imageSource = '/UnoCards/wildp4blue.png';
+                    elseif encodedCardNumber == 60
+                        obj.color = 'yellow';
+                        obj.imageSource = '/UnoCards/wildp4yellow.png';
+                    elseif encodedCardNumber == 61
+                        obj.color = 'green';
+                        obj.imageSource = '/UnoCards/wildp4green.png';
+                    end
                 end
-                obj.imageSource = char("/UnoCards/" + string(obj.color) + string(obj.number) + ".png");
+                if encodedCardNumber < 54
+                    obj.imageSource = char("/UnoCards/" + string(obj.color) + string(obj.number) + ".png");
+                end
             end       
         end
     end
